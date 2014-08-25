1e2db029e93b06c784dde02ef055ee6855cb67b4
lisptick <lisptick@gmail.com>
2014-08-25 16:09:47 +0200

    Typos in abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,20 +6,20 @@ class Ability
       #Except in demo mode
       # a user can create a project in an organization if he is an admin or he is the organization creator
       (the_user.demo? and !Organization.real.include? organization) or
-      	the_user.admin? or organization.users[1] == the_user
+       the_user.admin? or organization.users[0] == the_user
     end
 
     can :update, Project do |project|
       #Except in demo mode
       # a user can edit a project if he is an admin or he's the project creator or he participates to the project
       (user.demo? and !Organization.real.include? project.organization) or
-        user.admin? or project.organization.users[1] == user or
+        user.admin? or project.organization.users[0] == user or
         user.projects.include? project
     end
 
     can :destroy, Project do |project|
       !Organization.real.include? project.organization or
-        user.admin? or project.organization.users[1] == user or
+        user.admin? or project.organization.users[0] == user or
         user.projects.include? project
     end
 
@@ -29,12 +29,12 @@ class Ability
 
     can :update, Organization do |organization|
       (user.demo? and !Organization.real.include? organization)
-      user.admin? or organization.users[1] == user
+      user.admin? or organization.users[0] == user
     end
 
     can :destroy, Organization do |organization|
       (user.demo? and !Organization.real.include? organization)
-      user.admin? or organization.users[1] == user
+      user.admin? or organization.users[0] == user
     end
   end
 end

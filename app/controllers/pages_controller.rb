b4fb3ac9da28f22c1e9c9f60902ed15fe48ecbc9
lisptick <lisptick@gmail.com>
2014-09-09 23:38:43 +0200

    Removing the original home page, redirecting the user to his dashboard if logged, to the login page if not

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,16 +4,13 @@ class PagesController < ApplicationController
   before_filter :set_format
 
   def home
-    if user_signed_in?
-      flash.keep
-      if organization_id = cookies[:organization]
-        redirect_to organization_path(organization_id)
+    if organization_id = cookies[:organization]
+      redirect_to organization_path(organization_id)
+    elsif current_user
+      if current_user.organizations.empty?
+        redirect_to new_organization_path
       else
-        if current_user.organizations.empty?
-          redirect_to new_organization_path
-        else
-          redirect_to organization_path(current_user.organizations.first)
-        end
+        redirect_to organization_path(current_user.organizations.first)
       end
     end
   end
